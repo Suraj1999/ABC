5a9d5302b4dda623a7d680a7411bc48710332aaf
Suraj1999 <102948555+Suraj1999@users.noreply.github.com>
2023-12-14 23:05:39 +0530

    Add files via upload

---
+++ Cupcake/Cupcake/CupcakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeApp.swift
+//  Cupcake
+//
+//  Created by Suraj Gupta on 12/07/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
